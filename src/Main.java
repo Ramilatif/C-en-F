5838b5e18d4847e8678d256538ced73b4bd6fced
Ramilatif <latiframi18@gmail.com>
2023-03-30 09:57:32 +0200

    Save 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,47 @@
+import java.util.Scanner;
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        char an = 'O';
+        while (an == 'O')
+        {
+            System.out.println("1 - Vous voulez convertir du F° au C°\n"+
+                    "2 - Vous voulez convetire du C° au F°");
+            Scanner sc = new Scanner(System.in);
+
+            int t = sc.nextInt();
+            while(t!=1 && t!=2)
+            {
+                t = sc.nextInt();
+            }
+            if(t == 1)
+            {
+                System.out.print("Saissisez votre valeur F°:");
+
+                float x = sc.nextFloat();
+                float y = (x - 32) * 5 / 9;
+                System.out.println("La convertion en C° vaut "+y+"°");
+            }
+            else if(t == 2)
+            {
+                System.out.print("Saissisez votre valeur C°:");
+
+                float x = sc.nextFloat();
+                float y = (float) (x*1.8+32);
+                System.out.println("La convertion en F° vaut " + y+"°");
+            }
+            System.out.println("Voulez-vous reutiliser le programme? (O/N)");
+            sc.nextLine();
+            an =  sc.nextLine().charAt(0);
+            while (an != 'O' && an != 'N')
+            {
+                an =  sc.nextLine().charAt(0);
+            }
+        }
+        System.out.println("Au revoir!");
+
+
+
     }
 }
